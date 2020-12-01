4551bb64e83b3fdf737d875e56c399587a3e8f02
Joost Molenaar <jjm@j0057.nl>
2020-12-01 16:33:40 +0100

    [01] Report Repair: use operator instead of lambda

--- python/day01.py
+++ python/day01.py
@@ -1,10 +1,11 @@
 from itertools import combinations
 from functools import reduce
+import operator
 
 def report_repair(expenses, *, n=2):
     for entries in combinations(expenses, n):
         if sum(entries)== 2020:
-            return reduce(lambda a, b: a * b, entries, 1)
+            return reduce(operator.mul, entries, 1)
 
 def test_01_ex1():
     assert report_repair([1721, 979, 366, 299, 675, 1456]) == 514579
