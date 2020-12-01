96b91fdb8c15ddca65ba500513b89ff0f741da4e
Joost Molenaar <jjm@j0057.nl>
2020-12-01 08:46:16 +0100

    [01] Report Repair: refactor for part two

--- python/day01.py
+++ python/day01.py
@@ -1,9 +1,10 @@
 from itertools import combinations
+from functools import reduce
 
-def report_repair(expenses):
-    for (a, b) in combinations(expenses, 2):
-        if a + b == 2020:
-            return a * b
+def report_repair(expenses, *, n=2):
+    for entries in combinations(expenses, n):
+        if sum(entries)== 2020:
+            return reduce(lambda a, b: a * b, entries, 1)
 
 def test_01_ex1():
     assert report_repair([1721, 979, 366, 299, 675, 1456]) == 514579
