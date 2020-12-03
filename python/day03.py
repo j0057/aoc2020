564aff93821070c3aa2d6fc87a8f6e053a63d8b9
Joost Molenaar <jjm@j0057.nl>
2020-12-03 18:28:21 +0100

    [03] Toboggan Trajectory: re-implement using `more_itertools.iterate`

--- python/day03.py
+++ python/day03.py
@@ -1,24 +1,15 @@
+from itertools import takewhile
+from more_itertools import iterate
 import math
 
-def parse(lines):
-    return {x + y * 1j
-            for (y, line) in enumerate(lines)
-            for (x, char) in enumerate(line)
-            if char == '#'}, len(lines[0]), len(lines)
+def traverse_map(trees, y, x, dy, dx):
+    path = iterate(lambda p: (p[0] + dy, (p[1] + dx) % len(trees[0])), start=(0, 0))
+    return sum(trees[y][x] == '#' for (y, x) in takewhile(lambda p: p[0] < len(trees), path))
 
-def traverse_map(trees, w, h, pos, vec):
-    count = 0
-    while pos.imag < h:
-        count += pos in trees
-        pos += vec
-        if pos.real >= w:
-            pos = complex(pos.real % w, pos.imag)
-    return count
+def day03a(lines): return traverse_map(lines, 0, 0, 1, 3)
 
-def day03a(lines): return traverse_map(*parse(lines), 0+0j, 3+1j)
-
-def day03b(lines): return math.prod(traverse_map(*parse(lines), 0+0j, v)
-                                    for v in [1+1j, 3+1j, 5+1j, 7+1j, 1+2j])
+def day03b(lines): return math.prod(traverse_map(lines, 0, 0, dy, dx)
+                                    for (dy, dx) in [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)])
 
 ex=['..##.......',
     '#...#...#..',
@@ -31,10 +22,6 @@ ex=['..##.......',
     '#.##...#...',
     '#...##....#',
     '.#..#...#.#']
-
-def test_03_parse1(): assert 2+0j in parse(ex)[0]
-def test_03_parse2(): assert parse(ex)[1] == 11
-def test_03_parse3(): assert parse(ex)[2] == 11
 
 def test_03_ex1(): assert day03a(ex) == 7
 def test_03_ex2(): assert day03b(ex) == 336
