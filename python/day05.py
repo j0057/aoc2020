5084e19270e8ad4c8da245b64ac8ee79b595a5ff
Joost Molenaar <jjm@j0057.nl>
2020-12-05 06:22:42 +0100

    [05] Binary Boarding: solve part two

--- python/day05.py
+++ python/day05.py
@@ -2,9 +2,14 @@ def seat_id(spec):
     return int(spec.replace('F', '0').replace('B', '1').replace('R', '1').replace('L', '0'), 2)
 
 def day05a(specs): return max(seat_id(s) for s in specs)
+def day05b(specs):
+    seats = {seat_id(s) for s in specs}
+    return next(s for s in range(max(seats)+1)
+                if s not in seats and s+1 in seats and s-1 in seats)
 
 def test_05_ex1(): assert seat_id('BFFFBBFRRR') == 567
 def test_05_ex2(): assert seat_id('FFFBBBFRRR') == 119
 def test_05_ex3(): assert seat_id('BBFFBBFRLL') == 820
 
 def test_05a(day05_lines): assert day05a(day05_lines) == 919
+def test_05b(day05_lines): assert day05b(day05_lines) == 642
