a1118661abda8663636b2c4893bb22bb6994a3c3
Joost Molenaar <jjm@j0057.nl>
2020-12-07 07:07:49 +0100

    [07] Handy Haversacks: solve part two
    Fuck it: off by one is good enough for me

--- python/day07.py
+++ python/day07.py
@@ -14,11 +14,18 @@ def find(bags, T):
         return c == T or any(find(k) for (n, k) in bags[c])
     return sum(find(k) for k in bags if k != T)
 
+def count(bags, c, i=''):
+    return 1 + sum(n * count(bags, k) for n,k in bags[c])
+
 def day07a(bags): return find(dict(bags), 'shiny gold')
+def day07b(bags): return count(dict(bags), 'shiny gold')-1
 
 def test_07_ex1(): assert day07a(parse(ex1)) == 4
+def test_07_ex2(): assert day07b(parse(ex1)) == 32
+def test_07_ex3(): assert day07b(parse(ex2)) == 126
 
 def test_07a(day07_lines): assert day07a(parse(day07_lines)) == 287
+def test_07b(day07_lines): assert day07b(parse(day07_lines)) == 48160
 
 ex1 = ['light red bags contain 1 bright white bag, 2 muted yellow bags.',
        'dark orange bags contain 3 bright white bags, 4 muted yellow bags.',
@@ -29,3 +36,11 @@ ex1 = ['light red bags contain 1 bright white bag, 2 muted yellow bags.',
        'vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.',
        'faded blue bags contain no other bags.',
        'dotted black bags contain no other bags.']
+
+ex2 = ['shiny gold bags contain 2 dark red bags.',
+       'dark red bags contain 2 dark orange bags.',
+       'dark orange bags contain 2 dark yellow bags.',
+       'dark yellow bags contain 2 dark green bags.',
+       'dark green bags contain 2 dark blue bags.',
+       'dark blue bags contain 2 dark violet bags.',
+       'dark violet bags contain no other bags.']
