dad54ae05b02efd3b1dede67592251ffb08bc261
Joost Molenaar <jjm@j0057.nl>
2020-12-08 09:00:53 +0100

    [08] Handheld Halting: solve part two
    (with especially hacky hacking)

--- python/day08.py
+++ python/day08.py
@@ -33,13 +33,36 @@ class LoopVM:
                 trace |= {self.ip}
             self.ip += op(*args)
 
+    def reset(self):
+        self.__init__()
+        self.code = self.code_[:]
+
+    def hack_jmp_nop(self):
+        self.code_ = self.code[:]
+        for (i, (op, args)) in enumerate(self.code_):
+            self.reset()
+            if op == self._op_nop:
+                self.code[i] = (self._op_jmp, args)
+            elif op == self._op_jmp:
+                self.code[i] = (self._op_nop, args)
+            self.trace_loop()
+            if self.ip == len(self.code):
+                break
+
 def day08a(lines):
     vm = LoopVM.parse(lines)
     vm.trace_loop()
     return vm.acc
 
+def day08b(lines):
+    vm = LoopVM.parse(lines)
+    vm.hack_jmp_nop()
+    return vm.acc
+
 ex1 = 'nop +0|acc +1|jmp +4|acc +3|jmp -3|acc -99|acc +1|jmp -4|acc +6'.split('|')
 
 def test_08_ex1(): assert day08a(ex1) == 5
+def test_08_ex2(): assert day08b(ex1) == 8
 
 def test_08a(day08_lines): assert day08a(day08_lines) == 1782
+def test_08b(day08_lines): assert day08b(day08_lines) == 797
