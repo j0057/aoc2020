97a30940b3e2210cf0c79239816c3ec87d783e6f
Joost Molenaar <jjm@j0057.nl>
2020-12-12 11:13:34 +0100

    [11] Seating System: solve part two

--- python/day11.py
+++ python/day11.py
@@ -5,7 +5,7 @@ def parse(lines):
                           for (x, state) in enumerate(line)
                           if state == 'L'}
 
-def iterate(state, max_r):
+def iterate(state, max_r, t):
     N = defaultdict(lambda: [])
     for (y, x) in state:
         for dy in [-1, 0, +1]:
@@ -19,12 +19,12 @@ def iterate(state, max_r):
                         break
     while True:
         state = {(y, x): '#' if c == 0 else
-                         'L' if c >= 4 else state[y, x]
+                         'L' if c >= t else state[y, x]
                  for (y, x, c) in ((y, x, [state[n] for n in N[y, x]].count('#')) for (y, x) in state)}
         yield state
 
-def day11a(state, max_r=1):
-    for state_ in iterate(state, max_r):
+def day11a(state, max_r=1, tolerance=4):
+    for state_ in iterate(state, max_r, tolerance):
         if state == state_:
             return sum(v == '#' for v in state_.values())
         state = state_
@@ -35,5 +35,7 @@ def test_11_parse1(): assert parse(ex1).get((0, 0), '.') == 'L'
 def test_11_parse2(): assert parse(ex1).get((0, 1), '.') == '.'
 
 def test_11_ex1(): assert day11a(parse(ex1)) == 37
+def test_11_ex2(): assert day11a(parse(ex1), max_r=10, tolerance=5) == 26
 
 def test_11a(day11_lines): assert day11a(parse(day11_lines)) == 2126
+def test_11b(day11_lines): assert day11a(parse(day11_lines), max_r=90, tolerance=5) == 1914
