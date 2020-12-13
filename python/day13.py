bb9a7a4a0ad9ee0049ae6d7050dc5eade9d43b21
Joost Molenaar <jjm@j0057.nl>
2020-12-13 14:07:29 +0100

    [13] Shuttle Search: attempt solution for part two...
    ...using brute-force solution that will probably not terminate before
    heat death of the universe.

--- python/day13.py
+++ python/day13.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 def parse(lines):
     start = int(lines[0])
     buses = [int(x) if x.isnumeric() else None for x in lines[1].split(',')]
@@ -8,8 +10,22 @@ def day13a(start, buses):
     wait = first - (start % first)
     return first * wait
 
+def day13b(buses):
+    return next(t for t in count(start=0, step=buses[0])
+                if all(b - (t % b) == i
+                       for (i, b) in enumerate(buses)
+                       if i and b))
+
 def test_13_ex1(): assert day13a(*parse(ex1)) == 295
 
+def test_13_ex2a(): assert day13b(parse(ex1)[1]) == 1068781
+def test_13_ex2b(): assert day13b([17, None, 13, 19]) == 3417
+def test_13_ex2c(): assert day13b([67, 7, 59, 61]) == 754018
+def test_13_ex2d(): assert day13b([67, None, 7, 59, 61]) == 779210
+def test_13_ex2e(): assert day13b([67, 7, None, 59, 61]) == 1261476
+def test_13_ex2f(): assert day13b([1789, 37, 47, 1889]) == 1202161486
+
 def test_13a(day13_lines): assert day13a(*parse(day13_lines)) == 2045
+def _est_13b(day13_lines): assert day13b( parse(day13_lines)[1]) == None
 
 ex1 = ['939', '7,13,x,x,59,x,31,19']
