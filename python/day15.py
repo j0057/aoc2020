295682dfe250b6d499ed8bf93538820e82d7f3c3
Joost Molenaar <jjm@j0057.nl>
2020-12-15 06:43:24 +0100

    [15] Rambunctious Recitation: solve part two in 18.79s
    RAMBUNCTIOUS, adj.: marked by uncontrollable exuberance, unruly

--- python/day15.py
+++ python/day15.py
@@ -17,6 +17,7 @@ def memory_game(initial):
         yield n
 
 def day15a(initial): return nth(memory_game(initial), 2020-1)
+def day15b(initial): return nth(memory_game(initial), 30000000-1)
 
 def test_15_ex0(): assert [*islice(memory_game([0, 3, 6]), 0, 10)] == [0, 3, 6, 0, 3, 3, 1, 0, 4, 0]
 
@@ -28,3 +29,4 @@ def test_15_ex5(): assert day15a([3, 2, 1]) == 438
 def test_15_ex6(): assert day15a([3, 1, 2]) == 1836
 
 def test_15a(day15_text): assert day15a(parse(day15_text)) == 1294
+def test_15b(day15_text): assert day15b(parse(day15_text)) == 573522
