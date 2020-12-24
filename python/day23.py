d4227ace7f1c706739929bcb6fa3cff9a8c32b4b
Joost Molenaar <jjm@j0057.nl>
2020-12-24 21:20:32 +0100

    [23] Crab Cups: solve part two, fast

--- python/day23.py
+++ python/day23.py
@@ -1,50 +1,56 @@
-from collections import deque
-
 def parse(text):
-    return deque(int(x) for x in text.strip())
+    return [int(x) for x in text.strip()]
+
+def out(cups, cur):
+    s = []
+    while cur not in s:
+        s.append(cur)
+        cur = cups[cur]
+    return s
 
 def play(cups, moves):
-    for _ in range(moves):
-        cups.rotate(-1)
+    cur = cups[0]
+    cups = dict(zip(cups, cups[1:] + [cups[0]]))
+    highest = max(cups)
 
-        pickup = [cups.popleft() for _ in range(3)]
+    for _ in range(1, moves+1):
+        # pick up next 3 cups
+        pickup = [cups[cur], cups[cups[cur]], cups[cups[cups[cur]]]]
 
-        cur = dest = cups[-1]
-        while True:
-            dest = dest - 1 if dest >= 2 else 9
-            if dest not in pickup:
-                break
+        # remove them from the circle
+        cups[cur] = cups[pickup[-1]]
 
-        while cups[0] != dest:
-            cups.rotate(-1)
+        # select destination cup: cur-1, wrap around to highest and skip those picked up
+        dest = cur-1 if cur>1 else highest
+        while dest in pickup:
+            dest = dest-1 if dest>1 else highest
 
-        cups.rotate(-1)
-        cups.extendleft(reversed(pickup))
+        # insert picked up cups back in circle
+        cups[pickup[-1]] = cups[dest]
+        cups[dest] = pickup[0]
 
-        while cups[0] != cur:
-            cups.rotate(-1)
-        cups.rotate(-1)
+        # select new current cup
+        cur = cups[cur]
 
     return cups
 
 def day23a(cups, moves):
     cups = play(cups, moves)
-    while cups[0] != 1:
-        cups.rotate(-1)
-    cups.popleft()
-    return ''.join(str(x) for x in cups)
+    return ''.join(map(str, out(cups, 1)))[1:]
 
-def day23b(cups):
+def day23b(cups, moves):
     cups.extend(range(10, 1_000_000+1))
-    cups = play(cups, 10_000_000)
-    while cups[0] != 1:
-        cups.rotate(-1)
-    cups.rotate(-1)
-    return cups.popleft() * cups.popleft()
+    print(len(cups))
+    cups = play(cups, moves)
+    print(cups[1])
+    print(cups[cups[1]])
+    return cups[1] * cups[cups[1]]
 
 def test_23_ex1(): assert day23a(parse('389125467'), 10)  == '92658374'
 def test_23_ex2(): assert day23a(parse('389125467'), 100) == '67384529'
 
-def test_23_ex3(): assert day23b(parse('389125467')) == 149245887792
-
 def test_23a(day23_text): assert day23a(parse(day23_text), 100) == '25368479'
+
+def test_23_ex3(): assert day23b(parse('389125467'), 10_000_000) == 149245887792
+
+def test_23b(day23_text): assert day23b(parse(day23_text), 10_000_000) == 44541319250
